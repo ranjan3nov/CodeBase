c5007b650c7cae6bd48a0e0a51037c9503d77dbd
Alok Ranjan <ranjan3nov@gmail.com>
2025-04-24 20:13:28 +0530

    Vue started

---
+++ Vue/vue-crash-course/src/App2.vue
@@ -0,0 +1,48 @@
+<!-- Options API -->
+<script>
+export default{
+  data(){
+    return{
+      name: 'Ranjan',
+      status: 'pending',
+      tasks: ['Task One', 'Task Two', 'Task Three'],
+      link: "https://google.com"
+    };
+  },
+  methods:{
+    toggleStatus(){
+      if(this.status === 'active'){
+        this.status = 'pending';
+      }else if (this.status === 'pending'){
+        this.status = 'inactive';
+      }
+      else{
+        this.status = 'active';
+      }
+    }
+  }
+};
+</script>
+
+<template>
+  <h1>{{name}}</h1>
+  <p v-if="status === 'active'">
+    User is active
+  </p>
+  <p v-else-if="status === 'pending'">
+    user is pending
+  </p>
+  <p v-else>
+    User is inactive
+  </p>
+
+  <ul>
+    <li v-for="task in tasks" :key="task  ">
+      {{task}}
+    </li>
+  </ul>
+
+  <a v-bind:href="link">Google</a> <br>
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
+</template>
