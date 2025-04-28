3b912da5432591c94d186ae4830c604dfe411a2b
Alok Ranjan <ranjan3nov@gmail.com>
2025-04-28 15:26:54 +0530

    fixed tailwind issue

---
+++ Vue/vue-crash-course/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto p-4">
+    <h1 class="text-3xl font-bold text-blue-600">Vue Jobs</h1>
+    <p class="mt-2 text-gray-600">Welcome to the Vue Jobs application!</p>
+    <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Click me
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
