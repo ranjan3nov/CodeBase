b3a88de5b88d1469568826a6a769ef0e64252a21
Alok Ranjan <ranjan3nov@gmail.com>
2025-05-06 23:38:58 +0530

    vue crash completed

--- Vue/vue-crash-course/src/views/JobView.vue
+++ Vue/vue-crash-course/src/views/JobView.vue
@@ -3,10 +3,15 @@
   import BackButton from '@/components/BackButton.vue';
 
   import { reactive, onMounted } from 'vue';
-  import { useRoute, RouterLink } from 'vue-router';
+  import { useRoute, RouterLink, useRouter } from 'vue-router';
+  import {useToast} from 'vue-toastification'
+
   import axios from 'axios';
   
   const route = useRoute();
+  const router = useRouter();
+  const toast = useToast();
+
   const jobId = route.params.id
 
   const state = reactive({
@@ -14,6 +19,20 @@
     isLoading: true
   });
 
+  const deleteJob = async () => {
+    try{
+      const confirm = window.confirm('Are you sure you want to delete this job?');
+      if(!confirm) return;
+      await axios.delete(`/api/jobs/${jobId}`);
+      toast.success('Job deleted successfully');
+      router.push('/jobs');
+    }
+    catch(error){
+      console.log(error)
+      toast.error('Error deleting job')
+    }
+  }
+  
   onMounted(async ()=>{
     try {
       const res = await axios.get(`/api/jobs/${jobId}`);
@@ -24,6 +43,7 @@
       state.isLoading = false;
     }
   })
+
 </script>
 
 <template>
@@ -41,9 +61,7 @@
               <div
                 class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
               >
-                <i
-                  class="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
-                ></i>
+                <i class="pi pi-map-marker text-orange-700 mr-2"></i>
                 <p class="text-orange-700">{{state.job.location}}</p>
               </div>
             </div>
@@ -96,7 +114,7 @@
                 class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</RouterLink
               >
-              <button
+              <button @click="deleteJob"
                 class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
